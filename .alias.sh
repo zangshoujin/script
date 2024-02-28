c4fe497b5fa68feb3ee7c8236b8c73ae028b0af5
ubuntu-unraid <zangshoujin@126.com>
2024-02-28 22:28:57 +0800

    add Unraid ubuntu .alias.sh

--- .alias.sh
+++ .alias.sh
@@ -11,4 +11,6 @@ if [ `whoami` == "ubuntu" ]; then
     alias zs='cd /home/ubuntu/Code/scripts'
 elif [ `whoami` == "centos" ]; then
     alias zs='cd /home/centos/Code/scripts'
+elif [ `whoami` == "zangshoujin" ]; then
+    alias zs='cd /home/zangshoujin/Code/scripts'
 fi
